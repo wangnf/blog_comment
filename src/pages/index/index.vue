83e6344fd443a013cb014cdc9166a2b7be955cf5
wnf <990752800@qq.com>
2019-06-09 00:39:36 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="app">
+    <div>{{name}}</div>
+    <ul>
+      <li v-for="item in list" :key="item">{{item}}</li>
+    </ul>
+    <div>{{count}}</div>
+    <button @click="increment">点击</button>
+    <picker mode="date" :value="date" start="2015-09-01" end="2017-09-01" @change="bindDateChange">
+      <view class="picker">当前选择: {{date}}</view>
+    </picker>
+    <button class="btn" @click="navTo">跳转到log</button>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+import { toTabPage } from '@/utils/index'
+export default {
+  data () {
+    return {
+      count: 0,
+      name: 'wnf',
+      list: ['a', 'b', 'c']
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    increment () {
+      this.count++
+    },
+    bindDateChange (data) {
+      console.log(data.mp.detail.value)
+      this.data = data.mp.detail.value
+    },
+    navTo () {
+      toTabPage('../logs/main')
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+.app {
+  background: #999;
+  .btn {
+    width: 300rpx;
+  }
+}
+</style>
